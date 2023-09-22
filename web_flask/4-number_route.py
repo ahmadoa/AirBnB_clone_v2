f990e536ae134e253efbc2c0e90037dbe8743a1b
ahmadoa <ouladaouida@gmail.com>
2023-09-22 11:36:13 -0700

    added /number/<int:n> route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""starts a flask web app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """prints Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def ctext(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_is_cool(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>")
+def num(n):
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
