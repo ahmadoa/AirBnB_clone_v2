ff0de5d809e951226c64009471790deba0e1e36a
ahmadoa <ouladaouida@gmail.com>
2023-09-22 12:19:45 -0700

    added

--- models/state.py
+++ models/state.py
@@ -3,15 +3,29 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    # For DBStorage
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all,\
-            delete-orphan", backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
