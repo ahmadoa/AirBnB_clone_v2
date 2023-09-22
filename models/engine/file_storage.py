482548f0c9216bfaf4c53d6ccc3047f4a9ede3a9
ahmadoa <ouladaouida@gmail.com>
2023-09-22 12:18:07 -0700

    updated the engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,3 +58,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
             self.save()
+
+    def close(self):
+        """deserializes json file to objects"""
+        self.reload()
